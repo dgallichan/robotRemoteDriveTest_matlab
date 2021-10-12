40db2b87541b6895aa51c6d3bab7598d1cb1936e
Daniel Gallichan <dgallichan@yahoo.com>
2021-10-12 16:27:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    radio.sendString(serial.readLine())
+    basic.showIcon(IconNames.Heart)
+    textLine = serial.readLine().split(":")
+    radio.sendValue(textLine[0], parseFloat(textLine[1]))
+    basic.pause(1000)
+    basic.showIcon(IconNames.Ghost)
 })
+let textLine: string[] = []
 radio.setGroup(99)
 basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
